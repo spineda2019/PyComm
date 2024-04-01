894bfbe7e6afb3aaa8ee0c2f16502696725507fb
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-01 11:57:46 -0400

    make port a command line arg

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include <ostream>
+
 #include "SerialMessenger.hpp"
 
-int main() {
-    sp::SerialMessenger messenger("/dev/ttyS1");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Use: test <COM PORT>" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Port Requested: " << argv[1] << std::endl;
+
+    sp::SerialMessenger messenger(argv[1]);
     return 0;
 }
